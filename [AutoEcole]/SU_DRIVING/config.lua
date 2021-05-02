b23f27281a5d4ab4a339f176aa633c72ebc1329d
UkiyoYB <83373150+UkiyoYB@users.noreply.github.com>
2021-05-02 15:40:49 +0200

    Add files via upload

---
+++ [AutoEcole]/SU_DRIVING/config.lua
@@ -0,0 +1,252 @@
+Config                 = {}
+Config.DrawDistance    = 100.0
+Config.MaxErrors       = 5
+Config.SpeedMultiplier = 3.6
+Config.Locale          = 'fr'
+
+Config.Prices = {
+	dmv         = 500,
+	drive       = 2500,
+	drive_bike  = 3000,
+	drive_truck = 5000
+}
+
+Config.VehicleModels = {
+	drive       = 'blista',
+	drive_bike  = 'sanchez',
+	drive_truck = 'mule3'
+}
+
+Config.SpeedLimits = {
+	residence = 50,
+	town      = 80,
+	freeway   = 120
+}
+
+Config.Zones = {
+
+	DMVSchool = {
+		Pos   = {x = 216.555, y = -1397.555, z = 29.555},
+		Size  = {x = 1.5, y = 1.5, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Type  = 1
+	},
+
+	VehicleSpawnPoint = {
+		Pos   = {x = 222.8, y = -1387.86, z = 30.4094, h = 270.31},
+		Size  = {x = 1.5, y = 1.5, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Type  = -1
+	}
+
+}
+
+Config.CheckPoints = {
+
+	{
+		Pos = {x = 255.139, y = -1400.731, z = 29.537},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			DrawMissionText(_U('next_point_speed', Config.SpeedLimits['residence']), 5000)
+		end
+	},
+
+	{
+		Pos = {x = 271.874, y = -1370.574, z = 30.932},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			DrawMissionText(_U('go_next_point'), 5000)
+		end
+	},
+
+	{
+		Pos = {x = 234.907, y = -1345.385, z = 29.542},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			Citizen.CreateThread(function()
+				DrawMissionText(_U('stop_for_ped'), 5000)
+				PlaySound(-1, 'RACE_PLACED', 'HUD_AWARDS', false, 0, true)
+				FreezeEntityPosition(vehicle, true)
+				Citizen.Wait(4000)
+
+				FreezeEntityPosition(vehicle, false)
+				DrawMissionText(_U('good_lets_cont'), 5000)
+			end)
+		end
+	},
+
+	{
+		Pos = {x = 217.821, y = -1410.520, z = 28.292},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			setCurrentZoneType('town')
+
+			Citizen.CreateThread(function()
+				DrawMissionText(_U('stop_look_left', Config.SpeedLimits['town']), 5000)
+				PlaySound(-1, 'RACE_PLACED', 'HUD_AWARDS', false, 0, true)
+				FreezeEntityPosition(vehicle, true)
+				Citizen.Wait(6000)
+
+				FreezeEntityPosition(vehicle, false)
+				DrawMissionText(_U('good_turn_right'), 5000)
+			end)
+		end
+	},
+
+	{
+		Pos = {x = 178.550, y = -1401.755, z = 27.725},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			DrawMissionText(_U('watch_traffic_lightson'), 5000)
+		end
+	},
+
+	{
+		Pos = {x = 116.650, y = -1355.800, z = 28.080}, 
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			DrawMissionText(_U('go_next_point'), 5000)
+		end
+	},
+
+	{
+		Pos = {x = 69.240, y = -1281.500, z = 28.070},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			DrawMissionText(_U('stop_for_passing'), 5000)
+			PlaySound(-1, 'RACE_PLACED', 'HUD_AWARDS', false, 0, true)
+			FreezeEntityPosition(vehicle, true)
+			Citizen.Wait(6000)
+			FreezeEntityPosition(vehicle, false)
+		end
+	},
+
+	{
+		Pos = {x = 71.970, y = -1181.700, z = 28.120},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			DrawMissionText(_U('go_next_point'), 5000)
+		end
+	},
+
+	{
+		Pos = {x = 15.140, y = -1127.640, z = 27.620},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			DrawMissionText(_U('go_next_point'), 5000)
+		end
+	},
+
+	{
+		Pos = {x = -67.860, y = -1132.910, z = 24.600},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			DrawMissionText(_U('go_next_point'), 5000)
+		end
+	},
+
+	{
+		Pos = {x = -246.260, y = -1132.140, z = 21.820},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			DrawMissionText(_U('go_next_point'), 5000)
+		end
+	},
+
+	{
+		Pos = {x = -185.680, y = -920.710, z = 28.070},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			DrawMissionText(_U('go_next_point'), 5000)
+		end
+	},
+
+	{
+		Pos = {x = -142.620, y = -787.840, z = 31.120},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			DrawMissionText(_U('go_next_point'), 5000)
+		end
+	},
+
+	{
+		Pos = {x = -118.080, y = -739.860, z = 33.140},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			DrawMissionText(_U('go_next_point'), 5000)
+		end
+	},
+
+	{
+		Pos = {x = 3.280, y = -768.200, z = 30.420},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			DrawMissionText(_U('go_next_point'), 5000)
+		end
+	},
+
+	{
+		Pos = {x = 145.390, y = -813.890, z = 29.760},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			DrawMissionText(_U('go_next_point'), 5000)
+		end
+	},
+
+	{
+		Pos = {x = 264.720, y = -856.830, z = 27.900},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			DrawMissionText(_U('go_next_point'), 5000)
+		end
+	},
+
+	{
+		Pos = {x = 388.330, y = -864.350, z = 27.870},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			DrawMissionText(_U('go_next_point'), 5000)
+		end
+	},
+
+	{
+		Pos = {x = 403.290, y = -883.610, z = 27.980},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			setCurrentZoneType('residence')
+			DrawMissionText(_U('in_town_speed', Config.SpeedLimits['residence']), 5000)
+		end
+	},
+
+	{
+		Pos = {x = 400.000, y = -937.310, z = 27.940},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			DrawMissionText(_U('gratz_stay_alert'), 5000)
+			PlaySound(-1, 'RACE_PLACED', 'HUD_AWARDS', false, 0, true)
+		end
+	},
+
+	{
+		Pos = {x = 486.390, y = -956.350, z = 25.920},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			DrawMissionText(_U('go_next_point'), 5000)
+		end
+	},
+
+	{
+		Pos = {x = 498.910, y = -1114.380, z = 28.930},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			DrawMissionText(_U('go_next_point'), 5000)
+		end
+	},
+
+	{
+		Pos = {x = 497.220, y = -1244.130, z = 27.740},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			DrawMissionText(_U('go_next_point'), 5000)
+		end
+	},
+
+	{
+		Pos = {x = 353.430, y = -1307.250, z = 30.900},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			DrawMissionText(_U('go_next_point'), 5000)
+		end
+	},
+
+	{
+		Pos = {x = 294.370, y = -1379.200, z = 30.220},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			DrawMissionText(_U('go_next_point'), 5000)
+		end
+	},
+
+	{
+		Pos = {x = 237.400, y = -1411.580, z = 29.590},
+		Action = function(playerPed, vehicle, setCurrentZoneType)
+			ESX.Game.DeleteVehicle(vehicle)
+		end
+	}
+
+}
---
+++ [AutoEcole]/SU_DRIVING/html/questions.js
@@ -0,0 +1,46 @@
+var tableauQuestion = [
+	{
+		question: "Vous êtes au volant d'une voiture et le compteur de vitesse affiche 50 km/h. Que faites-vous en voyant le panneau sortie d'agglomération ?",
+		propositionA: "Vous ralentissez.",
+		propositionB: "Vous maintenez votre vitesse.",
+		propositionC: "Vous accélérez légèrement pour atteindre une vitesse comprise entre 60 km/h et 80 km/h.",
+		propositionD: "Vous accélérez fortement pour atteindre une vitesse supérieure à 80 km/h.",
+		reponse: "C"
+	},
+
+	{
+		question: "Je roule sur une route avec une ligne blanche qui sépare votre route et la route opposée, un véhicule roule doucement devant vous. Que faites-vous ?",
+		propositionA: "Vous pouvez uniquement dépasser les véhicules motorisés.",
+		propositionB: "Le dépassement d'un cycliste est toléré mais vous ne pouvez que chevaucher la ligne blanche.",
+		propositionC: "J'appelle mon éxaminateur pour lui demander ce que dois faire.",
+		propositionD: "En l'absence de panneaux limitant la vitesse et par temps de pluie, la vitesse est limitée à 90 km/h.",
+		reponse: "B"
+	},
+
+	{
+		question: "Quel est le bon comportement à adopter en présence du feu rouge ?",
+		propositionA: "Il faut ralentir et passer en l'absence de piétons.",
+		propositionB: "Il faut absolument s'arrêter.",
+		propositionC: "La LSPD saisi votre véhicule.",
+		propositionD: "Vous n'avez pas le temps vous passez",
+		reponse: "B"
+	},
+
+	{
+		question: "Un angent de la LSPD vous demande de vous arrêter. Que faite-vous ?",
+		propositionA: "Je l'écrase.",
+		propositionB: "Je m'arrête.",
+		propositionC: "Je lui fait un fuck et je me casse.",
+		propositionD: "J'accélère.",
+		reponse: "B"
+	},
+
+	{
+		question: "En cas de beau temps et sans indication particulière, circuler à 130 km/h sur l'autoroute est autorisé...",
+		propositionA: "Sur la voie le plus à droite.",
+		propositionB: "Sur la voie la plus à gauche.",
+		propositionC: "Sur toutes les voies.",
+		propositionD: "Sur aucune des voies.",
+		reponse: "C"
+	},
+]
